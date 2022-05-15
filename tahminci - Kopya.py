522b71fa75c53fa0c6c2f33737b882a622f6f406
eyer25pro <96254724+eyer25pro@users.noreply.github.com>
2022-05-15 22:24:15 +0300

    ödevlerimveysel hocanın verdiği ödevler kopyalamadan dolayı bazı sıkıntılar ola bilir
    veysel hocanın verdiği ödevler kopyalamadan dolayı bazı sıkıntılar ola bilir

---
+++ tahminci - Kopya.py
@@ -0,0 +1,26 @@
+import random
+
+sayi = random.randint(1,1000)
+sayac = 0
+
+print(sayi)
+
+while True:
+
+    tahmin1 = int(input("İlk tahmininizi giriniz:"))
+    tahmin2 = int(input("İkinci tahmininizi giriniz:"))
+    tahmin3 = int(input("Üçüncü tahmininizi giriniz:"))
+    toptahmin = tahmin1 + tahmin2 + tahmin3
+
+    sayac+=1
+
+    if toptahmin > sayi:
+        print("Daha küçük bir toplam giriniz!")
+        print()
+    elif toptahmin < sayi:
+        print("Daha büyük bir toplam giriniz!")
+        print()
+    else:
+        print("Tebrikler doğro bildiniz!")
+        print(f"{sayac} tahminde bildiniz!")
+        break
