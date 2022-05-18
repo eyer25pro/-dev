7a5c1eca76295d8a5f1bb57ddf7acf53f9eff9e1
eyer25pro <96254724+eyer25pro@users.noreply.github.com>
2022-05-18 17:43:56 +0300

    Add files via upload

---
+++ meslekman.py
@@ -0,0 +1,19 @@
+import random
+import time
+print("sayı ile cevap verile bilen her şeye sayı ile cevap verin")
+x=["türkiyenin baş kenti","türkiyedeki il sayısı","istanbulun ilçe sayısı ","dünyanın en büyük alanına sahip ülkesi","ERZURUMun ilçe sayısı","SİVASIN ilçe sayısı","doğan cüceloğlu fen lisesindeki sınıf sayısı kaçtır","türkiyenin kaç bölgesi vardır","ERZURUMUN PLAKASI NEDİR","SİVASIN plakası nedir ","türkiyenin en büyük nüfusa sahip şehri","dünyanın en çok nüfusa sahip ülkesi","dünyada kaç kıta vardır","2 artı 2 kaçtır","2 nin karesi kaçtır","doğan cüceloğlu fen lisesi hangi ilçede bulunmaktadır","1 İN KARESİ","1 +1","BİR YILDA KAÇ AY VARDIR","BİR YILDA KAÇ GÜN VARDIR","KAÇ YILDA BİR YILIN GÜNLÜK SAYISI 366 OLUR"]
+y=["ANKARA","81","39","RUSYA","20","17","14","7","25","58","İSTANBUL","ÇİN","7","4","4","KÜÇÜKÇEKMECE","1","2","12","365","4"]
+z=21
+v=0
+if v =< 10:
+    for i in ramdom.randint(z):
+    ı=x[i]
+    c=y[i]
+    a=input(ı)
+        if a==c:
+            print("doğru bildin")
+        else:
+            print("yanlış bildin",c)
+    del liste[z]
+    v+=1
+    z-=1
